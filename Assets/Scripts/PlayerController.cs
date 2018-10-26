e99b8cd6bb912bce4166a6fb6071acb5bff52546
Pascal <pascalruetz@googlemail.com>
2018-10-26 22:06:36 +0200

    Pascal PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,17 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    public float speed;
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void FixedUpdate () {
-        if (Input.GetKey(KeyCode.W))
-        {
-            GetComponent<Rigidbody2D>().AddForce(Input.GetAxis("Vertical");
-        }
-    }
+	void Update () {
+		
+	}
 }
