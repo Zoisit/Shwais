1d139e63310ae13a39f3a51b8346340562ec7c02
Pascal <pascalruetz@googlemail.com>
2018-10-26 22:23:38 +0200

    Pascal PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    public float speed;
+    private Rigidbody2D rb2d;
 	// Use this for initialization
 	void Start () {
-		
+        rb2d = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        GetComponent<Rigidbody2D>().AddForce(movement * speed);
+        if (Input.GetKey(KeyCode.W))
+        {
+            GetComponent<Rigidbody2D>().AddForce(movement * speed);
+        }
+        
+
+    }
 }
