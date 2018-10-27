6bcad7847ff1cbecb2a1b80ca2e2639f4f8ba718
Marcus <marcus.j.90@web.de>
2018-10-27 02:01:27 +0200

    Sound files
    Sound themese

---
+++ Assets/Scripts/Movement_test.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement_test : MonoBehaviour
+{
+
+    public float speed = 1;
+    private Rigidbody2D rb2d;
+    // Use this for initialization
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    public AudioSource deathSound;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        rb2d.velocity = new Vector2(0, 0);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb2d.velocity = new Vector2(0, 1 * speed);
+            deathSound.Play();
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb2d.velocity = new Vector2(-1 * speed, 0);
+
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb2d.velocity = new Vector2(1 * speed, 0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb2d.velocity = new Vector2(0, -1 * speed);
+        }
+
+
+
+    }
+
+}
