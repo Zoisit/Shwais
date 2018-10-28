75edf4dbd6a8c6314536f9d215a849adc18ded08
Pascal <pascalruetz@googlemail.com>
2018-10-28 14:34:42 +0100

    + new object sprites

---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour {
+
+    public float startingTime;
+    private float countingTime;
+	// Use this for initialization
+	void Start () {
+        countingTime = startingTime;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
