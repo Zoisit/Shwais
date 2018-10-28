c56d773897c04313ace4218875731544e69f1bb3
Unknown <smaragdeule@gmx.de>
2018-10-28 13:31:52 +0100

    Character Selection Screen
    And Graphics

---
+++ Assets/SelectingPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectingPlayer : MonoBehaviour {
+
+    private int player1 = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (player1 == 0)
+            {
+                player1++;
+                gameObject.transform.GetChild(0).GetChild(1).GetChild(0).gameObject.SetActive(false);
+                gameObject.transform.GetChild(0).GetChild(1).GetChild(1).gameObject.SetActive(true);
+            }
+        }
+	}
+}
